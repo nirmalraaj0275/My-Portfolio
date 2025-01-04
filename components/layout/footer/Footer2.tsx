3b9d83a494a322f2d9f5fe9f20afc027bcf2c6d0
nirmalraaj0275 <nirmalraaj0275@gmail.com>
2025-01-04 10:59:33 +0530

    done

--- components/layout/footer/Footer2.tsx
+++ components/layout/footer/Footer2.tsx
@@ -29,7 +29,7 @@ export default function Footer2() {
 								<a href="/#resume" className="fs-6"> Resume </a>
 								<a href="/#services" className="fs-6"> Services </a>
 								<a href="/#portfolio" className="fs-6"> Portfolio </a>
-								<a href="/#blog" className="fs-6"> Blog </a>
+								{/* <a href="/#blog" className="fs-6"> Blog </a> */}
 								<a href="/#contact" className="fs-6"> Contact </a>
 							</div>
 						</div>
